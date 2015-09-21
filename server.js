4ec768da1af5c2b30ff440dc91c230e833ce5c06
Kenneth Yee <Kenneth_yee2@yahoo.com>
2015-09-21 19:13:04 -0400

    changes again

--- server.js
+++ server.js
@@ -29,17 +29,6 @@ app.use(methodOverride(function (req, res) {
 	}
 }));
 
-pg.connect(connectionString, function(err, client) {
-  if (err) throw err;
-  console.log('Connected to postgres! Getting schemas...');
-
-  client
-    .query('SELECT table_schema,table_name FROM information_schema.tables;')
-    .on('row', function(row) {
-      console.log(JSON.stringify(row));
-    });
-});
-
 // app.set('port', (process.env.PORT || 3000));
 
 // app.listen(app.get('port'), function() {

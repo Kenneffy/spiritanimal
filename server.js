b3816a6fae3608847a373c541c609783102a1228
Kenneth Yee <Kenneth_yee2@yahoo.com>
2015-09-21 17:49:57 -0400

    connection string

--- server.js
+++ server.js
@@ -32,6 +32,17 @@ app.use(methodOverride(function (req, res) {
 //     console.log("App running on port : ", app.get('port'));
 // });
 
+pg.connect(process.env.DATABASE_URL, function(err, client) {
+  if (err) throw err;
+  console.log('Connected to postgres! Getting schemas...');
+
+  client
+    .query('SELECT table_schema,table_name FROM information_schema.tables;')
+    .on('row', function(row) {
+      console.log(JSON.stringify(row));
+    });
+});
+
 app.listen(3000);
 
 
@@ -53,17 +64,7 @@ app.get('/', function (req, res){
 	});
 });
 
-app.get('/db', function (request, response) {
-  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-    client.query('SELECT * FROM test_table', function(err, result) {
-      done();
-      if (err)
-       { console.error(err); response.send("Error " + err); }
-      else
-       { response.render('pages/db', {results: result.rows} ); }
-    });
-  });
-})
+
 
 app.post('/form', function (req, res){
 	var western = req.body.western 

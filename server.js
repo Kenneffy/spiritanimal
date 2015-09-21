0d6f17720b185d302542e123873a1155abc32bcc
Kenneth Yee <Kenneth_yee2@yahoo.com>
2015-09-21 18:48:46 -0400

    changes

--- server.js
+++ server.js
@@ -29,11 +29,11 @@ app.use(methodOverride(function (req, res) {
 	}
 }));
 
-app.set('port', (process.env.PORT || 3000));
+// app.set('port', (process.env.PORT || 3000));
 
-app.listen(app.get('port'), function() {
-    console.log("App running on port : ", app.get('port'));
-});
+// app.listen(app.get('port'), function() {
+//     console.log("App running on port : ", app.get('port'));
+// });
 
 
 // app.listen(3000);

6264b2d898cf25e234f08359739460ef9d1b62a9
Kenneth Yee <Kenneth_yee2@yahoo.com>
2015-09-21 19:09:41 -0400

    connectionz

--- server.js
+++ server.js
@@ -29,6 +29,17 @@ app.use(methodOverride(function (req, res) {
 	}
 }));
 
+pg.connect(process.env.DATABASE_URL, function(err, client) {
+  if (err) throw err;
+  console.log('Connected to postgres! Getting schemas...');
+
+  client
+    .query('SELECT table_schema,table_name FROM information_schema.tables;')
+    .on('row', function(row) {
+      console.log(JSON.stringify(row));
+    });
+});
+
 // app.set('port', (process.env.PORT || 3000));
 
 // app.listen(app.get('port'), function() {
